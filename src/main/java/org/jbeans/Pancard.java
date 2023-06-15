3800cea2f3bdbe9eac2639f84f230c271f163416
AntonysamyChelladurai <68181796+AntonysamyChelladurai@users.noreply.github.com>
2023-06-15 18:36:33 +0530

    Override hashcode  Commit

--- src/main/java/org/jbeans/Pancard.java
+++ src/main/java/org/jbeans/Pancard.java
@@ -8,7 +8,7 @@ import lombok.Getter;
  */
 
 @Getter
-public final class Pancard
+final class Pancard
 {
     private final String id;
     private final String name;
@@ -28,6 +28,10 @@ public final class Pancard
                 ", lname='" + lname + '\'' +
                 '}';
     }
+    @Override
+    public int hashCode() {
+        return name == null ? 0 : name.hashCode();
+    }
 
     public static void main(String[] args )
     {
@@ -42,9 +46,14 @@ public final class Pancard
         String s1="Antony";
         String s2="Antony";
         String s3="samy";
+        String s4=new String("Antony");
+        String s5=new String("Antony");
+        //String s3="samy";
 
         System.out.println("S1 :: "+ s1+ "  "+s1.hashCode());
         System.out.println("S2 :: "+ s2+ "  "+s2.hashCode());
         System.out.println("S3 :: "+ s3+ "  "+s3.hashCode());
+        System.out.println("S4 :: "+ s4+ "  "+s4.hashCode());
+        System.out.println("S5 :: "+ s5+ "  "+s5.hashCode());
     }
 }
